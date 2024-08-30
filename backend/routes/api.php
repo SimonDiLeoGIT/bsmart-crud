4d92052c1d7b1c1af704c29acef5d8a56ae1a178
SimonDiLeoGIT <saimondiley@gmail.com>
2024-08-30 10:44:58 -0300

    feat: crea crud para category

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,18 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Api\CategoryController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(CategoryController::class)->group(function () {
+    Route::get('/categories', 'index');
+    Route::post('/category', 'store');
+    Route::get('/category/{id}', 'show');
+    Route::put('/category/{id}', 'update');
+    Route::delete('/category/{id}', 'destroy');
 });
